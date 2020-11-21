0de9a24bea88240b6d69260cddd4da38c47f77d7
Karlenkko <48716383+Karlenkko@users.noreply.github.com>
2020-11-21 10:01:58 +0100

    feat(util): add history for chat server

---
+++ src/Util/HistoryHandler.java
@@ -0,0 +1,33 @@
+package Util;
+
+import java.io.*;
+
+public class HistoryHandler {
+
+    public static String readAll(){
+        File history = new File("history.txt");
+        BufferedReader reader = null;
+        StringBuffer allMsg = new StringBuffer();
+        try {
+            reader = new BufferedReader(new FileReader(history));
+            String oneLine = null;
+            while ((oneLine = reader.readLine()) != null) {
+                allMsg.append(oneLine + "\n");
+            }
+            reader.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return allMsg.toString();
+    }
+
+    public static void writeAMessage(String msg) {
+        try{
+            FileWriter writer = new FileWriter("history.txt",true);
+            writer.write(msg + "\n");
+            writer.close();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
